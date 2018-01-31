f7e976efb7de9f8695e5126d3915d2cc2e7d6889
Cons Bulaqueña <consbulaquena@gmail.com>
2018-01-31 14:37:22 +0800

    Add view controllers and fixed the segues
    Segues from login to tabview controller.

---
+++ Cohance/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  Cohance
+//
+//  Created by Cons Bulaqueña on 31/01/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
