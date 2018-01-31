54c9f3e1f5bd7827f31e55681c30876d7eea1a9c
Cons Bulaqueña <consbulaquena@gmail.com>
2018-01-31 20:18:02 +0800

    Add Navigation Controller in Storyboard

--- Cohance/HomeViewController.swift
+++ Cohance/HomeViewController.swift
@@ -13,8 +13,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //change color bg
-        view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
 
